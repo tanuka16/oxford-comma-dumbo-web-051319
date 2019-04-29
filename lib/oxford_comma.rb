e05b8c6a8651ce5962a13867960a493b25af2a4a
Tanuka Das <das.tanuka@yahoo.com>
2019-04-29 01:02:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+  #array to sring -- .join methods
+  return array.join('') if array.length == 1
 
+  if array.length == 2
+    array.join(' and ')
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"              #if the size is more than or equal to 3 add "and" just before the last string of the array
+    array.join(', ')                            #convert by joining comma
+  end
 end
+
+=begin
+if array.length == 2
+  array.insert(1, "and").join("")
+elsif array.length > 2
+  array.insert
+=end
