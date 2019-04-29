642f4699ca610775e5271a1fde05146e7444695d
Tanuka Das <das.tanuka@yahoo.com>
2019-04-29 01:00:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,13 +5,9 @@ def oxford_comma(array)
   if array.length == 2
     array.join(' and ')
   elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
+    array[-1] = "and #{array[-1]}"              #if the size is more than or equal to 
     array.join(', ')
-  else
-    puts 
   end
-
-
 end
 
 =begin
